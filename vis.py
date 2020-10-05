5ae9418f03dd32071483d55365f7d0d90aff7148
erwan.salle <erwan.salle@epita.fr>
2020-10-05 14:31:39 +0200

    adding plot function

--- vis.py
+++ vis.py
@@ -2,14 +2,17 @@
 
 import roblib as rb
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 fig = rb.figure()
 ax = rb.Axes3D(fig)
 
-
-
-
-
+def plot(x, y, z):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    c = x + y + z
+    ax.scatter(x, y, z, c=c)
 
 if __name__ == "__main__":
     with open("pts.csv", "r") as f:
