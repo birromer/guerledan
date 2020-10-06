1aeb50290f8e9736d0f17a2205bf570cb7831aa8
Bernardo Hummes <hummescmpa@gmail.com>
2020-10-06 10:04:03 +0200

    started pool simulation

---
+++ tour.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import roblib as rb
+import numpy as np
+
+
+def f(x,u):
+    x1, x2, x3, x4 = x[0], x[1], x[2], x[3]
+    x1dot = x4 * rb.cos(x3)
+    x2dot = x4 * rb.sin(x3)
+    x3dot = :
+
+
+
+if __name__ == "__main__":
+    pass
