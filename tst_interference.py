82215f12ea6a593082bb567d552eb3588fe853e4
Bernardo Hummes <hummescmpa@gmail.com>
2020-10-09 14:06:42 +0200

    started magnetic interference test

--- tst_interference.py
+++ tst_interference.py
@@ -9,26 +9,25 @@ import numpy as np
 
 if __name__ == "__main__":
     compdrv.init_compass()
-    serial_arduino, data_arduino = ardudrv.init_arduino_line()
     gps = gpsdrv.init_line()
+    serial_arduino, data_arduino = ardudrv.init_arduino_line()
 
-    speeds = [(0,0), (10,10), (20,20), (40,40), (60,60), (90,90), (120,120), (150,150)]
+    speeds = [(10,10), (20,20), (40,40), (60,60), (90,90), (120,120), (150,150)]
     measurements = {}  # measurements[speed] = ((cx, cy, cz), (gx, gy, gz)) compass and gps
 
     # -> alternative where compass stays with divergent value as long as there's a motor working
-    # get compass and gps measurements
-    cx, cy, cz = compdrv.read_compass()
-    gps_r = gpsdrv.read_gll(gps)
-    measurements[(0,0)] = ((cx, cy, cz), (gps_r[0], gps_r[2], gps_r[4]))
+    # follow the cap before <--------------
 
     for speed in speeds:
+        print("Rspeed:", speed[0], "- Lspeed:", speed[1])
         start_time = time.time()
         speed_mes = np.array([[0], [0], [0], [0], [0], [0]])
         while time.time() - start_time < 10:  # loop for 10 seconds
-            ardudrv.send_arduino_cmd_motor(serial_arduino, 34, 29)  # accelerates to speed
+            # control according to the cap
+            ardudrv.send_arduino_cmd_motor(serial_arduino, speed[0], speed[1])  # accelerates to speed
             if time.time() - start_time <= 1:
                 cx, cy, cz = compdrv.read_compass()
-                gps_r = gpsdrv.read_gll(ser)
+                gps_r = gpsdrv.read_gll(gps)
                 mes = np.array([[cx], [cy], [cz], [gps_r[0]], [gps_r[2]], [gps_r[4]]])
                 speed_mes = np.hstack((speed_mes, mes))
         # get mean and deviation of of compass measurements
